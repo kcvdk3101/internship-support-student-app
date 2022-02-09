3b0cf08212b949d5e78c9ab2c0a04a8c8e5219bd
Khoi Vuong <kcvdk2000@gmail.com>
2022-02-10 00:04:16 +0700

    create fetchAssets function in utils

---
+++ src/utils/FetchAssets.ts
@@ -0,0 +1,28 @@
+import { Asset } from 'expo-asset'
+import * as Font from 'expo-font'
+
+async function LoadAssets() {
+  const iconAssets = Asset.loadAsync([
+    require('../assets/images/icon-bolt.png'),
+    require('../assets/images/icon-company.png'),
+    require('../assets/images/icon-cv.png'),
+    require('../assets/images/icon-delete.png'),
+    require('../assets/images/icon-edit.png'),
+    require('../assets/images/icon-home.png'),
+    require('../assets/images/icon-logout.png'),
+    require('../assets/images/icon-person.png'),
+    require('../assets/images/icon-upload.png'),
+  ])
+  const fetchFonts = Font.loadAsync({
+    'OpenSans-Bold': require('../assets/fonts/OpenSans/OpenSans-Bold.ttf'),
+    'OpenSans-Regular': require('../assets/fonts/OpenSans/OpenSans-Regular.ttf'),
+    'OpenSans-SemiBold': require('../assets/fonts/OpenSans/OpenSans-SemiBold.ttf'),
+    'Roboto-Bold': require('../assets/fonts/Roboto/Roboto-Bold.ttf'),
+    'Roboto-Regular': require('../assets/fonts/Roboto/Roboto-Regular.ttf'),
+  })
+
+  await fetchFonts
+  await iconAssets
+}
+
+export default LoadAssets
